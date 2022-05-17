c783b1cda8090244bd4a4539cd0f2924d909d30a
michao11 <obarekmichal@gmail.com>
2022-05-17 12:16:45 +0200

    added User and Player

---
+++ Board.py
@@ -0,0 +1,21 @@
+class Board:
+    def __init__(self):
+        self.__board = [
+            [' ',' ',' ',' ']
+            [' ',' ',' ',' ']
+            [' ',' ',' ',' ']
+            [' ',' ',' ',' ']]
+
+    def Reset(self):
+        pass
+    
+    def Display(self):
+        for x in self.__board:
+            for y in x:
+                print("[", end="")
+                print(y, end="")
+                print("]", end="")
+            print()
+
+    def SetPosition(self, chr, x, y):
+        pass
