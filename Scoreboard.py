d19697e35a91353e16371e5f9a08f149afcb60bf
michao11 <obarekmichal@gmail.com>
2022-05-30 21:56:00 +0200

    added save

--- Scoreboard.py
+++ Scoreboard.py
@@ -2,21 +2,32 @@ class Scoreboard:
     def __init__(self):
         self.__scoreboard = {}
         self.FillTheScoreboard
+
     def FillTheScoreboard(self):
         file = open("scores.txt", "r")
         content = file.read()
+        content = content[:-1]
         file.close()
-        list_content = content.split(" ")
 
-        slownik = {}
+        list_content = content.split(" ")
 
         i = 0
         while i < len(list_content):
-            slownik[list_content[i]] = int(list_content[i + 1])
+            self.__scoreboard[list_content[i]] = int(list_content[i + 1])
             i += 2
-        print(slownik)
 
     def Display(self):
         for key in self.__scoreboard:
             print(key + " " + str(self.__scoreboard[key]))
 
+    def Save(self, p_name, p_points):
+        if p_name in self.__scoreboard:
+            if self.__scoreboard[p_name] < p_points:
+                self.__scoreboard[p_name] = p_points
+        else:
+            self.__scoreboard[p_name] = p_points
+
+        file = open("scores.txt", 'w')
+        for key in self.__scoreboard:
+            file.write(key + " " + str(self.__scoreboard[key]) + " ")
+        file.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import Game
 import Scoreboard
 
 S1 = Scoreboard.Scoreboard()
 S1.FillTheScoreboard()
 S1.Display()
+S1.Save("player10", 13)
